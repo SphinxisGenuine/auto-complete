750828f0b063821b480f530824bde8c9136c52e6
Sphinx <sphinxisgenuine@gmail.com>
2026-07-20 23:05:39 +0530

    Added delete word from trie option handled all  the cases

--- src/trie.ts
+++ src/trie.ts
@@ -12,12 +12,21 @@ class Trienode{
         this.isEndOfWord = false;
         this.freqency= -1;
     }
-    
+
 }
 export class Trie {
     private root:Trienode;
     constructor(){
         this.root=new Trienode;
+    }
+    private dfs(node:Trienode,suggestion:Suggestion[],currentword:string){
+            if (node.isEndOfWord){
+                suggestion.push({word:currentword,freqency:node.freqency})
+            }
+        for (const [childLetter,childNode] of Object.entries(node.children)){
+
+            this.dfs(childNode,suggestion,currentword+childLetter)
+        }
     }
     insert (word:string,freqency:number):void{
         let node =this.root;
@@ -42,26 +51,16 @@ export class Trie {
     }
     startsWith(prefix: string): boolean {
     let current = this.root;
-
     for (const char of prefix) {
         if (!current.children[char]) {
             return false;
         }
-
         current = current.children[char];
     }
 
     return true;
+
 }   
-    private dfs(node:Trienode,suggestion:Suggestion[],currentword:string){
-            if (node.isEndOfWord){
-                suggestion.push({word:currentword,freqency:node.freqency})
-            }
-        for (const [childLetter,childNode] of Object.entries(node.children)){
-            this.dfs(childNode,suggestion,currentword+childLetter)
-        }
-        
-    }
     getSuggestion(word:string){
         let current=this.root
         let arrayofSuggestion:Suggestion []=[];
@@ -74,5 +73,73 @@ export class Trie {
         let temp = current;
          this.dfs(temp,arrayofSuggestion,word);
          return arrayofSuggestion!
+
     }
+
+    
+
+    // for deltion we need 2 things first we till deopth of the node
+
+    //  and we have to keep track of the parent node
+
+     
+
+   private deleteWord(node: Trienode, word: string, index: number): boolean {
+
+    if (index === word.length) {
+
+        if (!node.isEndOfWord) {
+
+            return false;
+
+        }
+
+        node.isEndOfWord = false;
+
+        node.freqency = -1;
+
+        return Object.keys(node.children).length === 0;
+
+    }
+
+    const char = word[index];
+
+    const child = node.children[char!];
+
+    if (!child) {
+
+        return false;
+
+    }
+
+    const shouldDeleteChild = this.deleteWord(child, word, index + 1);
+
+
+
+    if (shouldDeleteChild) {
+
+        delete node.children[char!];
+
+    }
+
+
+
+    return (
+
+        !node.isEndOfWord &&
+
+        Object.keys(node.children).length === 0
+
+    );
+
+}
+
+    delete(word: string): void {
+
+    this.deleteWord(this.root, word, 0);
+
+    }
+
+
+
 }
