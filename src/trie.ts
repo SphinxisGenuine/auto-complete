7fa6a00ef255a28d5876678c921d90fa0c88b8ca
Sphinx <sphinxisgenuine@gmail.com>
2026-07-24 21:41:00 +0530

    Implemeted TopK in the in the orignal Ds Now we can totalyy avoid dfs to give the suggestion

--- src/trie.ts
+++ src/trie.ts
@@ -2,15 +2,20 @@ type Suggestion={
 word :string;
 freqency:number;
 }
-
+type Wordinfo ={
+    word:string;
+    freqency:number
+}
 class Trienode{
     children :Record<string,Trienode>={};
     isEndOfWord:boolean=false;
     freqency:number;
+    TopK:Wordinfo[];
     constructor() {
         this.children = {};
         this.isEndOfWord = false;
         this.freqency= -1;
+        this.TopK=[];
     }
 
 }
@@ -19,6 +24,8 @@ export class Trie {
     constructor(){
         this.root=new Trienode;
     }
+
+    //v1 needed it 
     private dfs(node:Trienode,suggestion:Suggestion[],currentword:string){
             if (node.isEndOfWord){
                 suggestion.push({word:currentword,freqency:node.freqency})
@@ -30,11 +37,16 @@ export class Trie {
     }
     insert (word:string,freqency:number):void{
         let node =this.root;
+        const wordInfo = {
+             word,
+              freqency
+            };
         for (const char of word){
             if (!node.children[char]){
                 node.children[char]=new Trienode()
             }
-        node=node.children[char];
+            node=node.children[char];
+            this.updateTopk(node,wordInfo)
         } 
         node.isEndOfWord=true;
         node.freqency=freqency
@@ -63,16 +75,13 @@ export class Trie {
 }   
     getSuggestion(word:string){
         let current=this.root
-        let arrayofSuggestion:Suggestion []=[];
         for (const char of word){
             if (!current.children[char]){
                 return []
             }
             current=current.children[char];
         }
-        let temp = current;
-         this.dfs(temp,arrayofSuggestion,word);
-         return arrayofSuggestion!
+         return current.TopK
 
     }
 
@@ -81,8 +90,64 @@ export class Trie {
     // for deltion we need 2 things first we till deopth of the node
 
     //  and we have to keep track of the parent node
+    private readonly Top_K=8;
+    private updateTopk(node:Trienode,wordobj:Wordinfo){
+        //is the word exist update its frequncy 
+         const existingIndex = node.TopK.findIndex(entry => entry.word === wordobj.word);
 
-     
+        if (existingIndex!=-1){
+            node.TopK[existingIndex]!.freqency = wordobj.freqency;
+            node.TopK.sort((a, b) => b.freqency - a.freqency);
+            return
+        }
+        // if it not full push simply push
+        if(node.TopK.length < this.Top_K) {
+            node.TopK.push(wordobj);
+
+        node.TopK.sort((a, b) => b.freqency - a.freqency);
+        return;
+        }
+
+        // if the above two condition fails that means the word is is not in the topk 
+        // what we need to do is check the freqency of smallest in the topk and update the list 
+        const last = node.TopK[node.TopK.length - 1]!;
+
+        if (wordobj.freqency > last.freqency) {
+            node.TopK[node.TopK.length - 1] =wordobj;
+        node.TopK.sort((a, b) => b.freqency - a.freqency);
+        console.log(node.TopK)
+        }
+       
+    }
+    RecordSelection(word:string):boolean{
+         let current=this.root
+         let cuurent2=current
+         let newfreqency
+         for (const char of word){
+            if (!cuurent2.children[char]){
+                return false
+            }
+            cuurent2=cuurent2.children[char];
+        } 
+        if (!cuurent2.isEndOfWord) {
+           return false;
+            }
+        cuurent2.freqency++
+        newfreqency=cuurent2.freqency
+            const wordobj:Wordinfo={word:word,freqency:newfreqency}
+        for (const char of word){
+            if (!current.children[char]){
+                return true
+            }
+            current=current.children[char];
+            this.updateTopk(current,wordobj)
+        }
+         return true
+    }
+
+
+
+    
 
    private deleteWord(node: Trienode, word: string, index: number): boolean {
 
